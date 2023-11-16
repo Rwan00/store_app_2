7d7c3056abe5436ae9e237e46904847cb85ed41a
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-11-16 23:31:39 +0200

    add image on stack in ProductCard

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,57 +1,83 @@
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
   static String id = "HomePage";
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart,color: Colors.black,),),
-        ],
-        backgroundColor:Theme.of(context).canvasColor,
-        elevation: 0,
-        centerTitle: true,
-        title: const Text("New Trend",style: TextStyle(color: Colors.black),),
-      ),
-      body: Center(
-        child: Container(
-          height: 110,
-          width: 210,
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 40,
-                color: Colors.grey.withOpacity(0.2),
-                spreadRadius: 0,
-                offset: const Offset(10,10)
-              ),
-            ],
-            borderRadius: BorderRadius.circular(10)
-          ),
-          child: const Card(
-            elevation: 10,
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0,horizontal: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text("HandBag Lv",style: TextStyle(color: Colors.grey,fontSize: 16),),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(r"$225",style: TextStyle(fontSize: 16),),
-                      Icon(Icons.favorite,color: Colors.red,)
-                    ],
-                  )
-                ],
-              ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.shopping_cart,
+              color: Colors.black,
             ),
           ),
+        ],
+        backgroundColor: Theme.of(context).canvasColor,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "New Trend",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Center(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: 110,
+              width: 210,
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    blurRadius: 40,
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 0,
+                    offset: const Offset(10, 10)),
+              ], borderRadius: BorderRadius.circular(10)),
+              child: const Card(
+                elevation: 10,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "HandBag Lv",
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            r"$225",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 70,
+                left: 105,
+                child: Image.network(
+              "https://www.pngall.com/wp-content/uploads/2/Bag-PNG-Picture.png",
+              height: 100,
+            ))
+          ],
         ),
       ),
     );
